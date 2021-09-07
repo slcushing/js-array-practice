2ff978818e5e74ee5104c957dca6132a307a39ed
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-07 17:38:03 -0400

    Number1

--- codewars.js
+++ codewars.js
@@ -2,7 +2,11 @@
 // Testing 1-2-3
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 
-
+var number=function(array){
+    return array.map((line, index) => {
+      return `${index + 1}: ${line}`
+    });
+  }
 
 
 // # 2
