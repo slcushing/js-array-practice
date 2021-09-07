46574e70fd7ddbb4c33d1e5ca49797dc61023b6f
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-07 17:54:47 -0400

    Number2 Filling Array Pt.1

--- codewars.js
+++ codewars.js
@@ -13,6 +13,14 @@ var number=function(array){
 // Filling an array (part 1)
 // https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
 
+const arr = N => {
+    const arr = [];
+  
+    for(let i = 0; i < N; i+=1) {
+      arr.push(i);
+    }
+  return arr;
+  };
 
 
 
