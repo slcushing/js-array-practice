8d1290bb133eeb5f509f3d148b1f2176b0761a76
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-07 19:11:39 -0400

    Number4 Mean Array - Round Down

--- codewars.js
+++ codewars.js
@@ -44,7 +44,13 @@ function flyBy(lamps,drone) {
 // Get the mean of an array
 // https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
 
-
+function getAverage(marks){
+    var total = 0;
+    for (var i = 0; i < marks.length; i++) {
+      total += marks[i];
+    }
+    return Math.floor(total/marks.length);
+  }
 
 
 // #5
